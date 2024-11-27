13a894fdce07d5108d7b4610dbbf2b7ed16f0b39
Justus Kilian Wolff <justus.wolff@web.de>
2024-11-27 22:10:01 +0100

    wip

--- main.py
+++ main.py
@@ -9,23 +9,19 @@ locations = load_locations("small").sample(100).values
 n_clusters = int(len(locations) * MOPTA_CONSTANTS["mu_charging"] / (2 * MOPTA_CONSTANTS["station_ub"]))
 
 
-mopta_solver = Solver(
+s = Solver(
     vehicle_locations=locations,
     loglevel=logging.INFO,
     service_level=0.95,
 )
 
 # compute number of initial locations
-mopta_solver.add_initial_locations(n_clusters, mode="k-means", seed=0)
-mopta_solver.add_initial_locations(n_clusters, mode="random")
-mopta_solver.add_samples(num=2)
-mopta_solver.solve()
+s.add_initial_locations(n_clusters, mode="k-means", seed=0)
+s.add_initial_locations(n_clusters, mode="random")
+s.add_samples(num=2)
+s.solve()
 
-print("Test")
-# v_sol_built, locations_built, mip_gap, mip_gap_relative, iterations = mopta_solver.solve(
-#     verbose=False, timelimit=10, epsilon_stable=100
-# )
-
+print('finished')
 # objective_values, build_cost, distance_cost, service_levels, mip_gaps = mopta_solver.allocation_problem(
 #     locations_built=locations_built, v_sol_built=v_sol_built, n_iter=10
 # )
