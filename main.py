ed87ccfa2870f27d5585d9a5284e313d9fda7057
Justus Kilian Wolff <justus.wolff@web.de>
2024-12-10 00:09:19 +0100

    need to debug

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from ev_station_solver.solving.validator import Validator
 
 logger = get_logger(__name__)
 
-
+ 
 # use given starting solutions
-locations = load_locations("small").sample(100).values
+locations = load_locations("small").values  # .values.sample(100).values
 n_clusters = int(len(locations) * MOPTA_CONSTANTS["mu_charging"] / (2 * MOPTA_CONSTANTS["station_ub"]))
 service_level = 0.95
 
@@ -19,8 +19,8 @@ s = Solver(vehicle_locations=locations, loglevel=logging.INFO, service_level=ser
 # compute number of initial locations
 s.add_initial_locations(n_clusters, mode="k-means", seed=0)
 s.add_initial_locations(n_clusters, mode="random")
-s.add_samples(num=1)
-location_solutions = s.solve()
+s.add_samples(num=2)
+location_solutions = s.solve(epsilon_stable=10000)
 
 best_sol = location_solutions[-1]  # take last solution (the one with optimal locations without filtering)
 
